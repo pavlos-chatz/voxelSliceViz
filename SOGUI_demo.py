ce45eef8c79cbf188e63e26d0022bcc9b9cfc2f1
pavlos-chatz <pavlos.chatzisavvas@gmail.com>
2021-11-05 17:23:13 +0200

    add requirements.txt

--- SOGUI_demo.py
+++ SOGUI_demo.py
@@ -1,6 +1,7 @@
 import numpy as np
 from SliceObserverGUI import SliceObserverGUI
 
+
 def demo():
     # create an array with the first axis being the axis along which you will move when visualizing
     shape = (10, 60, 60)
